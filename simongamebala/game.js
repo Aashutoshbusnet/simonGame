0626e06400623f4715734c37b8bb15512cb68334
Aashutoshbusnet <145586855+Aashutoshbusnet@users.noreply.github.com>
2023-11-11 09:21:29 +0545

    Add files via upload

--- simongamebala/game.js
+++ simongamebala/game.js
@@ -12,12 +12,12 @@ var level = 0;
 var startingGame = false;
 
 // starting game after clickig any letter in keyboard
-$(document).keypress(function () {
+$(".heading").click(function () {
   if (!startingGame) {
-    // this is for the wrong case scenerio 
+
     $(".main-container").removeClass("backgroundImage");
     $(".box").show();
-
+    
     autoPlayingSequence();
     startingGame = true;
   }
@@ -25,42 +25,45 @@ $(document).keypress(function () {
 
 // generating automatic playing pattern sequence so that it will look like computer is pressing button itself while playhing game
 function autoPlayingSequence() {
-    userPlayingPattern = [];
-    level++;
+  userPlayingPattern = [];
+  level++;
 
-    $(".heading").text("Level " + level);
+  $(".heading").text("Level " + level);
 
-    var randomNumber = Math.floor(Math.random() * 4); // Corrected this line
-    var autoColorPicker = colors[randomNumber];
+  var randomNumber = Math.floor(Math.random() * 4); // Corrected this line
+  var autoColorPicker = colors[randomNumber];
 
-    autoPlayingPattern.push(autoColorPicker);
-
-    pressingAnimation(autoColorPicker);
-    playSound(autoColorPicker);
+  autoPlayingPattern.push(autoColorPicker);
+  
+  pressingAnimation(autoColorPicker);
+  playSound(autoColorPicker);
 }
 
-// letting user to choose which button is pressed 
-$(".box").click(function (){
-    var userColorPicker = $(this).attr("id");
-    userPlayingPattern.push(userColorPicker);
+// letting user to choose which button is pressed
+$(".box").click(function () {
+  var userColorPicker = $(this).attr("id");
+  userPlayingPattern.push(userColorPicker);
 
-    pressingAnimation(userColorPicker);
-    playSound(userColorPicker);
+  pressingAnimation(userColorPicker);
+  playSound(userColorPicker);
 
-    checkingAnswer(userPlayingPattern.length-1);
-})
+  checkingAnswer(userPlayingPattern.length - 1);
+});
 
 // Checking the answer, does the sequence match with the automatic generated sequence and the user clicked sequence
-function checkingAnswer(range){
-    if(autoPlayingPattern[range] === userPlayingPattern[range]){
-        if (autoPlayingPattern.length === userPlayingPattern.length) {
-            setTimeout(() => {
-                autoPlayingSequence();
-            }, 1000);
-        }
+function checkingAnswer(range) {
+  if (autoPlayingPattern[range] === userPlayingPattern[range]) {
+    if (autoPlayingPattern.length === userPlayingPattern.length) {
+      setTimeout(() => {
+        
+        autoPlayingSequence();
+
+      }, 1000);
+    }
     } else {
         // $(".heading").text("Game over mother Fucker, Press A Key to Restart the Game");
-        $(".heading").text(" ");
+        $(".heading").text("Click me to Restart the game");
+       
         $(".main-container").addClass("backgroundImage");
         $(".box").hide();
         playSound("wrong");
@@ -69,18 +72,18 @@ function checkingAnswer(range){
         autoPlayingPattern = [];
         startingGame = false;
     }
-}
+  }
 
 // making a function for Pressing Animation
-function pressingAnimation(autoColorPicker){
-    $("#" + autoColorPicker).addClass("pressed");
-    setTimeout(function () {
-        $("#" + autoColorPicker).removeClass("pressed");
-    }, 100);
+function pressingAnimation(autoColorPicker) {
+  $("#" + autoColorPicker).addClass("pressed");
+  setTimeout(function () {
+    $("#" + autoColorPicker).removeClass("pressed");
+  }, 100);
 }
 
-// making a function for playing sound, so that it will be easier to add sound every time 
-function playSound(sound){
-    var soundName = new Audio('./sounds/' + sound + ".mp3");
-    soundName.play();
+// making a function for playing sound, so that it will be easier to add sound every time
+function playSound(sound) {
+  var soundName = new Audio("./sounds/" + sound + ".mp3");
+  soundName.play();
 }
